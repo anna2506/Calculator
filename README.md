395aa88a15f34dfb1818ca4243d6411ab45b215d
Anna Doshchenko <annadoshenko3@gmail.com>
2019-10-19 11:09:41 +0200

    Added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+Instrunental development tools lab "calculator"
+This programm ask you to eneter the first number, then second and then you need to eneter an operation. As a result you see an answer.
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,37 +3,70 @@ public class Calculator {
   int num1;
   int num2;
   char operation;
+  /**
+   * getting variable num1
+   * @return
+   */
   public int getNum1() {
     return num1;
   }
 
+  /**
+   * set variable num1
+   * @param num1
+   */
   public void setNum1(int num1) {
     this.num1 = num1;
   }
 
+  /**
+   * getting variable num2
+   * @return
+   */
   public int getNum2() {
     return num2;
   }
 
+  /**
+   * set variable num2
+   * @param num2
+   */
   public void setNum2(int num2) {
     this.num2 = num2;
   }
 
+  /**
+   * getting variable operation
+   */
   public char getOperation() {
     return operation;
   }
 
+  /**
+   * set variable operation
+   * @param operation
+   */
   public void setOperation(char operation) {
     this.operation = operation;
   }
 
-
+/**
+ * constructor
+ */
   public Calculator()
   {
     this.num1 = input.getNum1();
     this.num2 = input.getNum2();
     this.operation = input.getOperation();
   }
+
+  /**
+   * calculating num1 and num2 with an operation
+   * @param num1
+   * @param num2
+   * @param operation
+   * @return
+   */
   public int calc(int num1, int num2, char operation){
     int result = 0;
     switch (operation){
